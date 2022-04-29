8c6648e80b8da17f5857278bdbb3f23ca55ee861
AustinWu020815 <wu020707@gmail.com>
2022-04-29 15:07:03 +0800

    Box123 finish

--- src/com/fju/ai/Box2.java
+++ src/com/fju/ai/Box2.java
@@ -1,4 +1,10 @@
 package com.fju.ai;
 
-public class Box2 {
+public class Box2 extends Box {
+    public Box2() {
+        super(1, 2, 3);
+        length = 20;
+        width = 20;
+        height = 20;
+    }
 }
